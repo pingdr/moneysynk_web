7848407cd99fe663640571d84fd9f1284cd27112
Parth Gondaliya <gondaliyaparth66@gmail.com>
2020-10-14 20:08:51 +0530

    - validation in mobile number

--- src/app/shared/modals/enter-mobilenum/enter-mobilenum.component.ts
+++ src/app/shared/modals/enter-mobilenum/enter-mobilenum.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy, VERSION } from '@angular/core';
 import { TestService } from './test.service';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MatDialog } from '@angular/material/dialog';
 import { VerifyMobileOtpComponent } from '../verify-mobile-otp/verify-mobile-otp.component';
 import { HttpService } from 'src/app/services/http.service';
@@ -22,7 +22,7 @@ export class EnterMobilenumComponent implements OnInit {
   submitted = false;
   mobile: any;
   public loader = false;
-
+  isValidFormSubmitted = null;
   dialogRefofOtpModal: MatDialogRef<VerifyMobileOtpComponent>;
 
   public form: FormGroup;
@@ -42,10 +42,14 @@ export class EnterMobilenumComponent implements OnInit {
   // get f() { return this.addmobile.controls; }
 
   addmobiles() {
-    this.addmobile = this.formBuilder.group({
-      mobile_no: ['',Validators.maxLength(10)],
-      countryCode: ['']
-    });
+    // this.addmobile = this.formBuilder.group({
+    //   mobile_no: ['',Validators.maxLength(10),Validators.minLength(10)],
+    //   countryCode: ['']
+    // });
+    this.addmobile = new FormGroup({
+      mobile_no: new FormControl(''),
+      countryCode: new FormControl(''),
+    })
   }
 
   applyFilter(filterValue: any) {
@@ -57,9 +61,13 @@ export class EnterMobilenumComponent implements OnInit {
 
   }
   save() {
-    if (this.addmobile.invalid) {
+    this.isValidFormSubmitted = true;
+    console.log(this.addmobile.get('mobile_no').errors);
+    
+    if (this.addmobile.get('mobile_no').errors) {
       return;
-  }
+    }
+    
     this.dialogRef.close(this.dialogRef);
 
     this.loader = true;
